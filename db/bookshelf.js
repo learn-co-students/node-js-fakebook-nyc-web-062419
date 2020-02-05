010664cd5c05e38453ed32bdee533e2d225b50ae
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2020-02-05 20:02:32 +0000

    wip

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -1,6 +1,8 @@
 const config  = require('../../knexfile.js');
 const knex = require('knex')(config[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
+const User = bookshelf.model('User', {
+  tableName: 'users'})
 
 bookshelf.plugin('registry');
 
