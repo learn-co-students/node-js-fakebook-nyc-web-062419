e1b5e06261b15f7c400708fb216b776d7f3da7a4
adriennem76 <50639575+adriennem76@users.noreply.github.com>
2020-02-07 14:09:47 -0500

    Update bookshelf.js

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -3,7 +3,8 @@ const knex = require('knex')(config[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
 const User = bookshelf.model('User', {
   tableName: 'users'})
-
+const Friend = bookshelf.model('Friend', {
+  tableName: 'users'})
 bookshelf.plugin('registry');
 
 module.exports = bookshelf;
